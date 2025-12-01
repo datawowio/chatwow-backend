eb87c97e09b31088c09cc553f3a37e2e5a5f3a25
Khunakorn Johnson <khunakorn.j@datawow.io>
2025-12-01 10:48:39 +0700

    chore: fix line message

--- src/app/worker/line-event/line-event.constant.ts
+++ src/app/worker/line-event/line-event.constant.ts
@@ -1,15 +1,16 @@
 // user send
-export const LINE_SELECTION_MENU_KEYWORD = 'ฉันรักดาต้าว้าว';
+export const LINE_SELECTION_MENU_KEYWORD = 'คุยเรื่องอะไรกันดี!';
 
 // Invalid reply
-export const LINE_INVALID_MESSAGE_REPLY = 'ผมยังไม่รองรับข้อความประเภทนี้ครับ';
+export const LINE_INVALID_MESSAGE_REPLY =
+  'ขออภัย ผมยังไม่รองรับข้อความประเภทนี้ครับ..';
 export const LINE_INVALID_VERIFICATION_REPLY = 'รหัสยืนยันไม่ถูกต้อง';
 export const LINE_INVALID_PROJECT_SELECTION_REPLY =
-  'ไม่มี project ที่คุณเลือก โปรดลองอีกครั้ง';
+  'ไม่พบหัวข้อที่คุณเลือก โปรดกดเลือกหัวข้ออีกครั้งครับ';
 export const LINE_NO_PROJECT_REPLY =
-  'ดูเหมือนคุณจะไม่มี Project โปรดติดต่อ admin';
+  'บัญชีของคุณยังไม่ได้ผูกกับหัวข้อใดๆ โปรดติดต่อแอดมินเพื่อให้ช่วยตรวจสอบ';
 
 // success reply
 export const LINE_SUCCESS_VERIFICATION_REPLY = 'ยืนยันตัวตนสำเร็จ';
 export const LINE_SUCCESS_PROJECT_SELECTION_REPLY = (projectName: string) =>
-  `เลือก project ${projectName} สำเร็จคุยได้เลย`;
+  `เลือก หัวข้อ ${projectName} สำเร็จคุยได้เลย`;
